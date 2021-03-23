a62b8ae6535c6965e2eae6abaf28d0ff9fd5381c
Rachana-Baditha <76876502+Rachana-Baditha@users.noreply.github.com>
2021-03-23 11:32:58 +0530

    Update basics.py
    Fixed Impy_Info to Idfy_Info

--- basics.py
+++ basics.py
@@ -1,11 +1,11 @@
-import Impy_Info 
+import Idfy_Info 
 
 async def intro(chnl):
     
-    await chnl.send(Impy_Info.Introduction)
+    await chnl.send(Idfy_Info.Introduction)
     
 async def commands(chnl):
     
-    await chnl.send(Impy_Info.Commands)
+    await chnl.send(Idfy_Info.Commands)
 
 
